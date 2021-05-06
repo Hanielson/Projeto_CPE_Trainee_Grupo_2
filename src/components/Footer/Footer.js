1fc482dce106d3be5acc41eed043afc1acee75fc
Renan <renancastro@cpejr.com.br>
2021-05-06 01:51:20 -0300

    Comentários sobre o código

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,36 +3,44 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <div className="all">
-
+    <div className="all"> 
       <div className="top-box">
-
+        
+        {/*Logo que importada de um site, já que a ferramenta
+        que uso não tem suporte gratuito para importar*/}
         <div className="logo">
           <img src="https://i.ibb.co/CJ0TfSK/book-pedia-2.png" />
         </div>
 
+        {/*Links das outras páginas*/}
         <div className="navegue">
           <p className="textoCima" align="center">
             Navegue
           </p>
           <div className="links">
-            <p>Home</p>
+            <a href="https://i.ibb.co/CJ0TfSK/book-pedia-2.png">
+              <p>Home</p>
+            </a>
             <p>&nbsp;&nbsp;</p>
             <p>Livros</p>
             <p>&nbsp;&nbsp;</p>
             <p>Usuários</p>
           </div>
         </div>
-        
-        <img src="https://i.ibb.co/RHz9bry/linha-vertical-removebg-preview.png" className="linha" />
 
+        {/*GAMBIARRA! imagem para funcionar com linha vertical, já
+        que usar <hr/> está me trazendo problemas*/}
+        <img src="https://i.ibb.co/RHz9bry/linha-vertical-removebg-preview.png"className="linha"/>
+
+
+        {/*Contatos do site*/}
         <div className="contatos">
           <p className="textoCima" align="center">
             Contatos
           </p>
           <div className="contatosTexto" align="center">
             <p>Contato@email.com</p>
-            <div className="numeroTelefone" >
+            <div className="numeroTelefone">
               <p>+55 (77) 9 9999-9999</p>
               <p>&nbsp;&nbsp;</p>
               <p>+55 (31) 9 9898-9898</p>
@@ -41,7 +49,6 @@ function Footer() {
         </div>
 
       </div>
-
     </div>
   );
 }
