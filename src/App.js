aa5d949726fd09e831338f472a388e33d9e8f8d7
Luana Pina <luanapina@cpejr.com.br>
2021-05-06 12:25:53 -0300

    import do footer. modificações no footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route } from 'react-router'
 import Routes from './routes'
+import './global.css'
 
 export default function App() {
   return <Routes/>
--- src/Componentes/footer/footer.js
+++ src/Componentes/footer/footer.js
@@ -1,11 +1,51 @@
-import React from 'react'
-import './footer.css'
+  
+import React from "react";
+import "./footer.css";
 
-export default function Footer() {
-    return (
-        <footer className={'footer'} >
-            <p>Bookpedia - Leitura Compartilhada!</p>
-            <p><small>Copyright</small></p>
-        </footer>
-    )
+function Footer() {
+  return (
+    <div className="all"> 
+      <div className="top-box">
+        
+        {/*Logo que importada de um site, já que a ferramenta
+        que uso não tem suporte gratuito para importar*/}
+        <div className="logo">
+          <img src="https://i.ibb.co/CJ0TfSK/book-pedia-2.png" />
+        </div>
+
+        {/*Links das outras páginas*/}
+        <div className="navegue">
+          <h1 className="textoCima" align="center"> Navegue </h1>
+          <div className="links">
+            <a href="https://i.ibb.co/CJ0TfSK/book-pedia-2.png">
+              <p>Home</p>
+            </a>
+            <p>&nbsp;&nbsp;</p>
+            <p>Livros</p>
+            <p>&nbsp;&nbsp;</p>
+            <p>Usuários</p>
+          </div>
+        </div>
+
+        {/*GAMBIARRA! imagem para funcionar com linha vertical, já
+        que usar <hr/> está me trazendo problemas*/}
+        <img src="https://i.ibb.co/RHz9bry/linha-vertical-removebg-preview.png"className="linha"/>
+
+
+        {/*Contatos do site*/}
+        <div className="contatos">
+            <h1 className = "textoCima" align="center"> Contatos </h1>
+            <p>Contato@email.com</p>
+            <div className="numeroTelefone">
+              <p>+55 (77) 9 9999-9999</p>  
+              <p>&nbsp;&nbsp;</p>
+              <p>+55 (31) 9 9898-9898</p>
+            </div>
+        </div>
+
+      </div>
+    </div>
+  );
 }
+
+export default Footer;
