c279f53c2b5f71bca51611c00d78901a4956d2ee
Renan <renancastro@cpejr.com.br>
2021-05-07 19:17:44 -0300

    Criado a página cadastro
    Parte visual quase feita, faltando apenas ajustes de texto e responsividade. Além disso falta inserir as funções de cada input e botões

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,69 +1,84 @@
-import React, { useState } from 'react'
-import Logo from '../../assets/logo.png'
-import Header from '../../Componentes/header/index'
-import Footer from '../../Componentes/footer/index'
+import React from 'react';
+import './Cadastro.css';
+import Footer from "../../components/Footer";
 
-/**
- * @todo Tirar os resquicios de HTML puro desse arquivo
- */
- export default function Cadastro() {
-    const [usuario, setUsuario] = useState([])
 
-    function handleInfoChange(e) {
-        const key = e.target.value
-        const newUsuario = usuario
-        newUsuario[key] = e.target.value
-        setUsuario(newUsuario)
-    }
+function Cadastro() {
+  return (
+    <div>
+    <div className="header"> </div>
+    <div className="cadastro">
+      
+      {/* Botões e inputs*/}
+      <div className="botoes_inputs">
+        <div>
+          <div className="inputNomeSobrenome">
+            <p >Nome</p>
+            <input/>
+          </div>
+          <div className="inputNomeSobrenome">
+            <p className="nomeEspacamento">Sobrenome</p>
+            <input className="inputSobrenome"/>
+          </div>
+        </div>
 
-    return (
-        <div className={'Cadastro'}>
-            <Header />
-            <article className={'Page'}>
-                <br /><br /><br />
-                <h2 id={'Cadastro'}>CADASTRO</h2>
-                <div>
-                    <h1>Novo Usuário</h1>
-                    <input
-                        type={'text'}
-                        placeholder={'Nome'}
-                        name={'nome'}
-                        onChange={handleInfoChange}
-                    />
-                    <br />
-                    <input
-                        type={'text'}
-                        placeholder={'Email'}
-                        name={'email'}
-                        onChange={handleInfoChange}
-                    />
-                    <br />
-                    <input
-                        type={'password'}
-                        placeholder={'Senha'}
-                        name={'senha'}
-                        onChange={handleInfoChange}
-                    />
-                    <br />
-                    <input
-                        type={'text'}
-                        placeholder={'Endereço'}
-                        name={'endereço'}
-                        onChange={handleInfoChange}
-                    />
-                    <br />
-                    <input
-                        type={'text'}
-                        placeholder={'Se você fosse um livro, qual seria?'}
-                        name={'voceumlivro'}
-                        onChange={handleInfoChange}
-                    />
-                    <br />
-                    <br />
-                    <button>Entrar</button>
-                </div>
-            </article>
-            <Footer/>
-        </div >
-    )
+        <div>
+          <div>
+            <p>Rua</p>
+            <input className="inputRuaCidade" />
+          </div>
+          <div>
+            <p className="nomeEspacamento">Número</p>
+            <input className="inputNumeroUF"/>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <p>Cidade</p>
+            <input className="inputRuaCidade" />
+          </div>
+          <div>
+            <p className="nomeEspacamento">UF</p>
+            <input className="inputNumeroUF"/>
+          </div>
+        </div>
+
+        <p>E-mail</p>
+        <input className="inputEmailSenhaPergunta" />
+        <p>Senha</p>
+        <input type="password" className="inputEmailSenhaPergunta" />
+        <p>Qual seu livro de infância preferido?</p>
+        <input className="inputEmailSenhaPergunta" />
+
+        <div>
+          <div className="botaoCadastrar">
+            <button className ="botaoCadastrar">Cadastrar</button>
+          </div>
+          <div className="perguntaCadastro">
+            <p className="perguntaCadastrado" align="right">Já está cadastrado?</p>
+            <p className="perguntaLogin" align="right">Faça o login!</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Linha vertical */}
+      <div>
+        <img
+          src="https://i.ibb.co/RHz9bry/linha-vertical-removebg-preview.png"
+          className="linhas"
+        />
+      </div>
+
+      {/* Logo */}
+      <div>
+        <img src="https://i.ibb.co/CJ0TfSK/book-pedia-2.png" className="logos" />
+      </div>
+    </div>
+
+    <Footer/>
+  </div>
+  );
 }
+
+export default Cadastro;
