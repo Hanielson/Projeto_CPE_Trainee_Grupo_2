fd0f2443f5681f18b88983e3a48a2d4907e5bc99
Renan <renancastro@cpejr.com.br>
2021-05-10 10:44:42 -0300

    Footer finalizado e o andamento da página de cadastro
    Falta deixar os botões e inputs funcionais. E para melhor adequação da página de cadastro terá que ser feito o Header primeiro, para depois fazer os ajustes finais.

--- src/Componentes/footer/footer.js
+++ src/Componentes/footer/footer.js
@@ -1,6 +1,6 @@
 ﻿import React from "react";
 import { Link } from "react-router-dom";
-import "./footer.css";
+import "./Footer.css";
 
 
 function Footer() {
--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,83 +1,94 @@
-import React from 'react';
+﻿import React from 'react';
 import './Cadastro.css';
-import Footer from "../../Componentes/footer";
-
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
 
 function Cadastro() {
   return (
     <div>
-    <div className="header"> </div>
-    <div className="cadastro">
+      <div className="cadastro">
+        <div>
+          {' '}
+          <Header />
+        </div>
+
+        {/* Botões e inputs*/}
+        <div className="botoes_inputs">
+          <div>
+            <div className="inputNomeSobrenome">
+              <p>Nome</p>
+              <input />
+            </div>
+            <div className="inputNomeSobrenome">
+              <p className="espacamentoSobrenome">Sobrenome</p>
+              <input className="inputSobrenome" />
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <p>Rua</p>
+              <input className="inputRuaCidade" />
+            </div>
+            <div>
+              <p className="espacamentoNumeroUF">Número</p>
+              <input className="inputNumeroUF" />
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <p>Cidade</p>
+              <input className="inputRuaCidade" />
+            </div>
+            <div>
+              <p className="espacamentoNumeroUF">UF</p>
+              <input className="inputNumeroUF" />
+            </div>
+          </div>
+
+          <p>E-mail</p>
+          <input className="inputEmailSenhaPergunta" />
+          <p>Senha</p>
+          <input type="password" className="inputEmailSenhaPergunta" />
+          <p>Qual seu livro de infância preferido?</p>
+          <input className="inputEmailSenhaPergunta" />
+
+          <div>
+            <div className="botaoCadastrar">
+              <button className="botaoCadastrar">Cadastrar</button>
+            </div>
+            <div>
+              <p className="perguntaCadastrado" align="right">
+                Já está cadastrado?
+              </p>
+              <p className="perguntaLogin" align="right">
+                Faça o login!
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Linha vertical */}
+        <div>
+          <img
+            src="https://i.ibb.co/RHz9bry/linha-vertical-removebg-preview.png"
+            className="linhas"
+          />
+        </div>
+
+        {/* Logo */}
+        <div>
+          <img
+            src="https://i.ibb.co/CJ0TfSK/book-pedia-2.png"
+            className="logos"
+          />
+        </div>
+      </div>
+
+      <div className="Foot"> <Footer /> </div>
       
-      {/* Botões e inputs*/}
-      <div className="botoes_inputs">
-        <div>
-          <div className="inputNomeSobrenome">
-            <p >Nome</p>
-            <input/>
-          </div>
-          <div className="inputNomeSobrenome">
-            <p className="nomeEspacamento">Sobrenome</p>
-            <input className="inputSobrenome"/>
-          </div>
-        </div>
-
-        <div>
-          <div>
-            <p>Rua</p>
-            <input className="inputRuaCidade" />
-          </div>
-          <div>
-            <p className="nomeEspacamento">Número</p>
-            <input className="inputNumeroUF"/>
-          </div>
-        </div>
-
-        <div>
-          <div>
-            <p>Cidade</p>
-            <input className="inputRuaCidade" />
-          </div>
-          <div>
-            <p className="nomeEspacamento">UF</p>
-            <input className="inputNumeroUF"/>
-          </div>
-        </div>
-
-        <p>E-mail</p>
-        <input className="inputEmailSenhaPergunta" />
-        <p>Senha</p>
-        <input type="password" className="inputEmailSenhaPergunta" />
-        <p>Qual seu livro de infância preferido?</p>
-        <input className="inputEmailSenhaPergunta" />
-
-        <div>
-          <div className="botaoCadastrar">
-            <button className ="botaoCadastrar">Cadastrar</button>
-          </div>
-          <div className="perguntaCadastro">
-            <p className="perguntaCadastrado" align="right">Já está cadastrado?</p>
-            <p className="perguntaLogin" align="right">Faça o login!</p>
-          </div>
-        </div>
-      </div>
-
-      {/* Linha vertical */}
-      <div>
-        <img
-          src="https://i.ibb.co/RHz9bry/linha-vertical-removebg-preview.png"
-          className="linhas"
-        />
-      </div>
-
-      {/* Logo */}
-      <div>
-        <img src="https://i.ibb.co/CJ0TfSK/book-pedia-2.png" className="logos" />
-      </div>
     </div>
-
-    <Footer/>
-  </div>
   );
 }
 
