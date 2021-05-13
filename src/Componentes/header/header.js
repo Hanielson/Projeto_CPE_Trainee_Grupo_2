b9cf94e9316b9ddc4ed5f6ce05d7899f219fb56d
Felipe Costa <xmitox@live.com>
2021-05-12 23:56:52 -0300

    home

--- src/Componentes/header/header.js
+++ src/Componentes/header/header.js
@@ -2,18 +2,22 @@ import React from 'react'
 import Logo from '../../assets/logo.png'
 import './header.css'
 
+
 export default function Header() {
     return (
         <div id={'nav'} >
             <ul className={'navigation1'} >
                 <img id={'Logo'} src={Logo} alt={'Bookpedia_Logo'} />
+                <ul id={'buttons'}>
                 <li className={'nav1'}><a href={'/'}>Home</a></li>
                 <li className={'nav1'}><a href={'/cadastro'}>Cadastro</a></li>
                 <li className={'nav1'}><a href={'/login'}>Login</a></li>
                 <li className={'nav1'}><a href={'/livros'}>Livros</a></li>
                 <li className={'nav1'}><a href={'/perfil'}>Perfil</a></li>
                 <li className={'nav1'}><a href={'/usuarios'}>Usuarios</a></li>
+                </ul>
             </ul>
+           
         </div>
     )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,40 +2,81 @@ import React from 'react'
 import Logo from '../../assets/logo.png'
 import './home.css'
 import Header from '../../Componentes/header/index'
+import Carousel from 'react-elastic-carousel'
 import Footer from '../../Componentes/footer/index'
+import imagem1 from '../../assets/imagem1.png'
+import imagem2 from '../../assets/imagem2.png'
+import imagem3 from '../../assets/imagem3.png'
+import livro from '../../assets/livro.png'
+
+
+
 
 export default function Home() {
+
+
+
     return (
         <div className={'Home'}>
             <Header />
             <article className={'Page'}>
-                <br /><br /><br />
-                <h2 id={'Home'}>HOME</h2>
+                
+                <div className={'fundo_rosa'}>
+                    <img  id={'imagem1'} src={imagem1} alt={'Mulher lendo'}/>
+                    <p id={'frase_principal'}>
+                    “Caminhais em direção da solidão. Eu, não, eu tenho os livros.”
+                    <p id={'autor'}>Marguerite Duras</p>
+                    </p>
+                    
+                </div>
 
-                <h4 id={'Sobre'}>Sobre Nós</h4>
-                <p id="up">
+                <div className={'sobre'}>
+                <h1 id={'Sobre'} className={'titulo'}>Sobre Nós</h1>
+                <p id="up"  className={'texto'}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
                     fringilla libero vel nulla suscipit tempus. Donec at mauris nec enim pulvinar
                     laoreet. Ut sed ligula a leo venenatis bibendum eget at felis. Donec gravida, est at
                     ultrices scelerisque, ligula dui dignissim nisl, ullamcorper consectetur purus arcu
                     in lacus. Duis imperdiet erat nunc, et dignissim tortor viverra id.
                 </p>
-                <h4 id={'Importancia'}>Qual a importância da leitura?</h4>
-                <p id={'mid'}>
+                </div>
+                
+                <div className={'importancia'}>
+                
+                <img  id={'imagem2'} src={imagem2} alt={'Mulher lendo'}/>
+                <h1 id={'Importancia'} className={'titulo'}>Importância da leitura</h1>
+                <p id={'mid'}  className={'texto'}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla libero
                     vel nulla suscipit tempus. Donec at mauris nec enim pulvinar laoreet. Ut sed ligula
                     a leo venenatis bibendum eget at felis. Donec gravida, est at ultrices scelerisque,
-                    ligula dui dignissim nisl, ullamcorper consectetur purus arcu in lacus. Duis
-                    imperdiet erat nunc, et dignissim tortor viverra id. Vivamus et pretium sem.
-                    Suspendisse massa sapien, cursus nec ipsum sit amet, vulputate condimentum arcu.
-                    Mauris molestie eros eget cursus eleifend.
+                    ligula dui dignissim nisl
                 </p>
-                <h4 id={'Share'}>Como posso compartilhar?</h4>
-                <p id={'low'}>
+                </div>
+
+                <div className={'compartilhar'}>
+
+                <h1 id={'Share'} className={'titulo'}>Como posso compartilhar?</h1>
+                <p id={'low'}  className={'texto'}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla
                     libero vel nulla suscipit tempus. Donec at mauris nec enim pulvinar laoreet.
                     Ut sed ligula a leo venenatis bibendum eget at felis.
                 </p>
+                <img  id={'imagem3'} src={imagem3} alt={'Mulher lendo'}/>
+                
+                </div>
+
+                <h1 id={'lancamento'} className={'titulo'}>Lançamentos</h1>
+                <Carousel itemsToShow={3}>
+                <img  id={'livro'} src={livro} alt={'livro'}/>
+                <img  id={'livro'} src={livro} alt={'livro'}/>
+                <img  id={'livro'} src={livro} alt={'livro'}/>
+                <img  id={'livro'} src={livro} alt={'livro'}/>
+                <img  id={'livro'} src={livro} alt={'livro'}/>
+                <img  id={'livro'} src={livro} alt={'livro'}/>
+                <img  id={'livro'} src={livro} alt={'livro'}/>
+                <img  id={'livro'} src={livro} alt={'livro'}/>
+                </Carousel>
+                
             </article>
 
             <Footer/>
