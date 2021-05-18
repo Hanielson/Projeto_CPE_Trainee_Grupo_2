5b625603818a8de72149b2978417f6548720d4ca
Luana Pina <luanapina@cpejr.com.br>
2021-05-18 16:28:53 -0300

    Layout pág perfil/ funcionalidades botão edit/ modificações de edição na tela/ ajustes na area dos livros

--- src/Componentes/areaLivro/areaLivro.js
+++ src/Componentes/areaLivro/areaLivro.js
@@ -2,20 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./areaLivro.css";
 
-export default function AreaLivros() {
+export default function AreaLivros({ tituloLivro, autorLivro, linkFotoLivro, opiniaoUsuario, notaLivro }) {
     return (
         <div className={'areaLivro'}>
-            <img className={'fotoLivro'} src="https://i.ibb.co/d2hsjB2/image-7.png"/>
+            <img className={'fotoLivro'} src={linkFotoLivro}/>
             <div className={'dadosLivro'}>
-                <p className={'tituloLivro'}> O menino do dedo verde <a className={'notaLivro'}>Nota: 6,0 </a> </p>
-                <p className={'autorLivro'}> Maurice Druon </p>
-                <p className={'opiniaoUsuario'}> 
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt 
-                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
-                    in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                </p>
-                <a className={'saibaMais'}>Saiba mais</a>
+                <p className={'tituloLivro'}> {tituloLivro} <a className={'notaLivro'}> Nota: {notaLivro} </a> </p>
+                <p className={'autorLivro'}> {autorLivro} </p>
+                <p className={'opiniaoUsuario'}> {opiniaoUsuario} </p>
             </div>
         </div>
     )
