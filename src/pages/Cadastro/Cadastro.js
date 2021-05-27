4a98f74a63dcf3dc22ebdccbf431440cd805212a
Luana Pina <luanapina@cpejr.com.br>
2021-05-27 20:30:44 -0300

    Responsividade e modificações página de cadastro

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,108 +1,168 @@
-﻿import React, { useState } from 'react';
-import './Cadastro.css';
-import Footer from '../../Componentes/footer/index';
-import Header from '../../Componentes/header/index';
+﻿import React, { useState } from "react";
+import "./Cadastro.css";
+import api from "../../services/api";
+import Footer from "../../Componentes/footer/index";
+import Header from "../../Componentes/header/index";
+import { useHistory } from "react-router";
 
 function Cadastro() {
-  
-  const  [usuario, setUsuario] = useState();
-  {/* Caracteres que são aceitos no input type="number" */}
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [senha, setSenha] = useState();
+  const [frase, setFrase] = useState();
+  const [rua, setRua] = useState();
+  const [numero, setNumero] = useState();
+  const [estado, setEstado] = useState();
+  const [pais, setPais] = useState();
+  const [cidade, setCidade] = useState();
+
+  {
+    /* Caracteres que são aceitos no input type="number" */
+  }
   const [symbolsArr] = useState(["e", "E", "+", "-", ".", ","]);
 
-  function handleInputChange(e){
-    
-    const key = e.target.name;
-    
-    const newUsuario = {...usuario};
+  const history = useHistory();
 
-    newUsuario[key] = e.target.value;
-
-    setUsuario (newUsuario);
-    console.log(newUsuario);
+  async function cadastro() {
+    try {
+      const body = { username, email, senha, frase };
+      const response_usuario = await api.post("/usuario", body);
+      console.log(response_usuario);
+      const user_id = response_usuario.data.user_id;
+      const response_endereco = await api.post(`/endereco/${user_id}`, {
+        rua,
+        numero,
+        cidade,
+        estado,
+        pais,
+      });
+      history.push("/login");
+    } catch (error) {
+      console.warn(error);
+      alert(error.message);
+    }
   }
-  
+
   return (
     <div className="paginaCadastro">
-
-      <Header />
+      <div>
+        <Header />
+      </div>
 
       <div className="cadastro">
-
         {/* Botões e inputs*/}
         <div className="botoes_inputs">
-          <div>
-            <div className="inputNomeSobrenome">
+          <div className="apelidoRuaNumero">
+            <div>
               <p>Apelido</p>
-              <input name="apelido" onChange={handleInputChange}/>
+              <p className="fraseCadastro">Frase que te define</p>
             </div>
-            <div className="inputNomeSobrenome">
-              <p className="espacamentoSobrenome">Frase que te define</p>
-              <input className="inputSobrenome" name="frase" onChange={handleInputChange} />
+            <div>
+              <input
+                name="apelido"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <input name="frase" onChange={(e) => setFrase(e.target.value)} />
             </div>
           </div>
 
-          <div>
+          <div className="apelidoRuaNumero">
             <div>
               <p>Rua</p>
-              <input className="inputRuaCidade" name="rua" onChange={handleInputChange} />
+              <p className="textNumeroUF">Número</p>
             </div>
             <div>
-              <p className="espacamentoNumeroUF">Número</p>
-              <input className="inputNumeroUF" name="numero" onChange={handleInputChange} type="number" onKeyDown={e => symbolsArr.includes(e.key) && e.preventDefault()} />
+              <input
+                className="inputRuaCidade"
+                name="rua"
+                onChange={(e) => setRua(e.target.value)}
+              />
+              <input
+                className="inputNumeroUF"
+                name="numero"
+                onChange={(e) => setNumero(e.target.value)}
+                type="number"
+                onKeyDown={(e) =>
+                  symbolsArr.includes(e.key) && e.preventDefault()
+                }
+              />
             </div>
           </div>
 
-          <div>
+          <div className="apelidoRuaNumero">
             <div>
               <p>Cidade</p>
-              <input className="inputRuaCidade" name="cidade" onChange={handleInputChange}/>
+              <p className="textNumeroUF">UF</p>
             </div>
             <div>
-              <p className="espacamentoNumeroUF">UF</p>
-              <input className="inputNumeroUF" name="uf" onChange={handleInputChange} />
+              <input
+                name="cidade"
+                className="inputRuaCidade"
+                onChange={(e) => setCidade(e.target.value)}
+              />
+              <input
+                className="inputNumeroUF"
+                name="uf"
+                oonChange={(e) => setEstado(e.target.value)}
+              />
             </div>
           </div>
 
-          <p>País</p>
-          <input className="inputEmailSenhaPergunta" name="pais" onChange={handleInputChange} />
-          <p>E-mail</p>
-          <input type="email" className="inputEmailSenhaPergunta" name="email" onChange={handleInputChange}/>
-          <p>Senha</p>
-          <input type="password" className="inputEmailSenhaPergunta" name="senha" onChange={handleInputChange} />
+          <div className="inputEmailSenhaPergunta">
+            <p>País</p>
+            <input name="pais" onChange={(e) => setPais(e.target.value)} />
+            <p>E-mail</p>
+            <input
+              type="email"
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <p>Senha</p>
+            <input
+              type="password"
+              name="senha"
+              onChange={(e) => setSenha(e.target.value)}
+            />
+          </div>
 
-          <div>
-            <div className="botaoCadastrar">
-              <button className="botaoCadastrar">Cadastrar</button>
-            </div>
+          <div className="botaoCadastrar">
             <div>
-              <p className="perguntaCadastrado" align="right">
-                Já está cadastrado?
-              </p>
-              <p className="perguntaLogin" align="right">
-                Faça o login!
-              </p>
+              <button type="button" value="text" onClick={cadastro}>
+                Cadastrar
+              </button>
+            </div>
+
+            <div>
+              <p>Já está cadastrado?</p>
+              <a href={"/login"} className="perguntaLogin">
+                <p>Faça o login!</p>
+              </a>
             </div>
           </div>
         </div>
 
         {/* Linha vertical */}
-        <div>
+        <div className="linhaCentro">
           <img
+            alt="Linha vertical"
             src="https://i.ibb.co/RHz9bry/linha-vertical-removebg-preview.png"
             className="linhas"
           />
         </div>
 
         {/* Logo */}
-        <div>
+        <div className="logoCadastro">
           <img
-            src="https://i.ibb.co/CJ0TfSK/book-pedia-2.png"
-            className="logos"
+            alt="Logo do Bookpedia"
+            src="https://i.ibb.co/K5sBWhC/oie-nm4q-Dx-Tc-Xe-FN.png"
           />
         </div>
-        </div>
-          <Footer />
       </div>
+
+      <div>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
